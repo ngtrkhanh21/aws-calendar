d6019d3a808a5e5820359978e24a3783d38c25bf
Kienht7704 <kienht7704@gmail.com>
2025-12-05 00:11:27 +0700

    fix

--- calendar2/src/App.jsx
+++ calendar2/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Calendar from './pages/Calendar';
-import Callback from './pages/CallBack';
+import Callback from './pages/Callback';
 import Login from './pages/Login';  
 import './App.css';
 
