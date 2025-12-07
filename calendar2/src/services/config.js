c29f5509e1fb61e964e2401effcad11656fd8748
thanhduy2307 <truongtduy2307@gmail.com>
2025-12-07 22:50:37 +0700

    Update config.js
    sửa client ID

--- calendar2/src/services/config.js
+++ calendar2/src/services/config.js
@@ -2,7 +2,7 @@
 export const CONFIG = {
   region: 'ap-southeast-1',
   userPoolId: 'ap-southeast-1_TryyHPjm0',
-  clientId: '5dct7sk93a0unassp7komfpidq',
+  clientId: '4n3c6mr7drq1h20qdql36m1t2k',
   cognitoDomain: 'https://ap-southeast-1tryyhpjm0.auth.ap-southeast-1.amazoncognito.com',
   apiGatewayUrl: 'https://3igtxin245.execute-api.ap-southeast-1.amazonaws.com/prod',
   
