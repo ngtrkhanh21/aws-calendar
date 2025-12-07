cb7b48072f61791e471fef70affe3c5a296c9252
thanhduy2307 <truongtduy2307@gmail.com>
2025-12-07 23:08:58 +0700

    Update calendarService.js
    sửa lại đường dẫn api

--- calendar2/src/services/calendarService.js
+++ calendar2/src/services/calendarService.js
@@ -2,22 +2,22 @@ import api from './api.js';
 
 export const calendarService = {
   async getCalendars() {
-    const response = await api.get('/calendars');
+    const response = await api.get('/todos');
     return response.data;
   },
 
   async createCalendar(calendarData) {
-    const response = await api.post('/calendars', calendarData);
+    const response = await api.post('/todos', calendarData);
     return response.data;
   },
 
   async updateCalendar(id, calendarData) {
-    const response = await api.put(`/calendars/${id}`, calendarData);
+    const response = await api.put(`/todos/${id}`, calendarData);
     return response.data;
   },
 
   async deleteCalendar(id) {
-    const response = await api.delete(`/calendars/${id}`);
+    const response = await api.delete(`/todos/${id}`);
     return response.data;
   },
 };
