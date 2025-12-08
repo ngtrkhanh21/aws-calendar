eae736b64b50b868ea642fadbdbf9ec6a8ba9cb6
thanhduy2307 <truongtduy2307@gmail.com>
2025-12-08 10:22:31 +0700

    Update calendarService.js

--- calendar2/src/services/calendarService.js
+++ calendar2/src/services/calendarService.js
@@ -1,23 +1,23 @@
-import api from './api.js';
+import { apiTodo } from "./api"; 
 
 export const calendarService = {
   async getCalendars() {
-    const response = await api.get('/todos');
+    const response = await apiTodo.get('/todos');
     return response.data;
   },
 
   async createCalendar(calendarData) {
-    const response = await api.post('/todos', calendarData);
+    const response = await apiTodo.post('/todos', calendarData);
     return response.data;
   },
 
   async updateCalendar(id, calendarData) {
-    const response = await api.put(`/todos/${id}`, calendarData);
+    const response = await apiTodo.put(`/todos/${id}`, calendarData);
     return response.data;
   },
 
   async deleteCalendar(id) {
-    const response = await api.delete(`/todos/${id}`);
+    const response = await apiTodo.delete(`/todos/${id}`);
     return response.data;
   },
 };
