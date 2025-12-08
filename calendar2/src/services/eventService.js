e1903940811a19bd920ab9643af4190017ab9215
thanhduy2307 <truongtduy2307@gmail.com>
2025-12-08 10:22:34 +0700

    Update eventService.js

--- calendar2/src/services/eventService.js
+++ calendar2/src/services/eventService.js
@@ -1,28 +1,28 @@
-import api from './api.js';
+import { apiEvent } from "./api"; 
 
 export const eventService = {
   async getEvents(params = {}) {
-    const response = await api.get('/events', { params });
+    const response = await apiEvent.get('/events', { params });
     return response.data;
   },
 
   async getEvent(id) {
-    const response = await api.get(`/events/${id}`);
+    const response = await apiEvent.get(`/events/${id}`);
     return response.data;
   },
 
   async createEvent(eventData) {
-    const response = await api.post('/events', eventData);
+    const response = await apiEvent.post('/events', eventData);
     return response.data;
   },
 
   async updateEvent(id, eventData) {
-    const response = await api.put(`/events/${id}`, eventData);
+    const response = await apiEvent.put(`/events/${id}`, eventData);
     return response.data;
   },
 
   async deleteEvent(id) {
-    const response = await api.delete(`/events/${id}`);
+    const response = await apiEvent.delete(`/events/${id}`);
     return response.data;
   },
 };
